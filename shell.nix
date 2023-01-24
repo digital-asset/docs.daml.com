cd56e948ad65eebda3bbb65b57835ea91767f434
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2023-01-24 17:09:16 +0100

    delegate Python deps management to Python deps manager (#127)
    This should make it easier to add/play with Sphinx extensions moving
    forward.
    
    This still uses direnv to:
    - Load the Nix environment.
    - Load the Python virtualenv environment.
    - Ensure that any change to `requirements.txt` is immediately reflected
      (on next CLI prompt).
    
    This still uses Nix to:
    - Install appropriate versions of curl, AWS CLI, git, jq, imagemagick,
      and TeX (along with all of the required TeX packages).
    - Install Python itself, so we know we're all using the same one.

--- shell.nix
+++ shell.nix
@@ -1,45 +1,5 @@
 let
   pkgs = import (import ./nix/sources.nix).nixpkgs {};
-  sphinx-copybutton = pkgs.python3Packages.buildPythonPackage rec {
-      pname = "sphinx-copybutton";
-      version = "0.2.12";
-
-      src = pkgs.python3Packages.fetchPypi {
-        inherit pname version;
-        sha256 = "0p1yls8pplfg59wzmb96m3pjcyr3202an1rcr5wn2jwqhqvqi4ll";
-      };
-      doCheck = false;
-      buildInputs = [pkgs.python3Packages.sphinx];
-  };
-  sphinx-external-toc = pkgs.python3Packages.buildPythonPackage rec {
-      pname = "sphinx_external_toc";
-      version = "0.3.1";
-
-      src = pkgs.python3Packages.fetchPypi {
-        inherit pname version;
-        sha256 = "sha256-nI6pmA6g5Xvzzpj2pAD5tp6x34CPfdeWycjMGHPYs1U=";
-      };
-      doCheck = false;
-      buildInputs = with pkgs.python3Packages; [sphinx flit-core click pyyaml];
-      format = "pyproject";
-  };
-  sphinx-rli = pkgs.python3Packages.buildPythonPackage rec {
-      pname = "sphinxext-remoteliteralinclude";
-      version = "0.4.0";
-
-      src = pkgs.python3Packages.fetchPypi {
-        inherit pname version;
-        sha256 = "sha256-TbJaI8BOCVYMvl4Hh7AmMwXj8ygqI5aUXvXATBu5ejM=";
-      };
-      doCheck = false;
-      buildInputs = with pkgs.python3Packages; [sphinx six];
-  };
-  sphinx-exts = pkgs.python3Packages.sphinx.overridePythonAttrs (attrs: rec {
-    propagatedBuildInputs = attrs.propagatedBuildInputs ++ [
-      sphinx-copybutton
-      sphinx-external-toc
-      sphinx-rli];
-  });
   texlive = pkgs.texlive.combine {
     inherit (pkgs.texlive)
       bera
@@ -90,10 +50,10 @@ pkgs.mkShell {
     pkgs.awscli
     pkgs.bash
     pkgs.curl
+    pkgs.direnv
     pkgs.git
     pkgs.jq
     pkgs.imagemagick
-    sphinx-exts
     texlive
     pkgs.pipenv
     pkgs.python39
