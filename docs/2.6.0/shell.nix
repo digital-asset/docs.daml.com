f3f05d72b86a9333af3e6ef278c0741d3846504b
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2023-01-26 18:02:01 +0100

    [update root] move current version to 2.6.0 subdir
    In order to be able to update the root website, we need to be able to
    build the root website. This means that we need, at a minimum, two
    separate versions of the docs editable and buildable at the same time:
    the "next" version, on which most of the work happens, and the "current"
    version, on which we may want to make occasional adjustments.
    
    Right now the "next" version is 2.6.0 and the "current" one is 2.5.1.
    
    However, giving the docs repo the ability to update root essentially
    forces it to take over root updates generally, which means we'll benefit
    from being able to build more than just the last two versions. More on
    that later.
    
    For the time being, this PR moves the next version under a subfolder;
    the current version will be added as a subsequent PR, and then (as yet
    another PR) I'll add the root update itself.
    
    For most of the work, which touches only the next version, this should
    be a very minimal disruption as all it changes is the work happens in a
    nested folder, but everything stays the same.

---
+++ docs/2.6.0/shell.nix
@@ -0,0 +1,56 @@
+let
+  pkgs = import ./nix/nixpkgs.nix;
+  texlive = pkgs.texlive.combine {
+    inherit (pkgs.texlive)
+      bera
+      capt-of
+      collection-fontsrecommended
+      collection-luatex
+      datetime
+      enumitem
+      environ
+      epigraph
+      eqparbox
+      eulervm
+      fancyhdr
+      fmtcount
+      fncychap
+      footmisc
+      footnotebackref
+      framed
+      latexmk
+      lipsum
+      mathpartir
+      mathpazo
+      mnsymbol
+      multirow
+      needspace
+      palatino
+      scheme-small
+      tabulary
+      threeparttable
+      tikzsymbols
+      titlesec
+      tocbibind
+      todonotes
+      transparent
+      trimspaces
+      varwidth
+      wrapfig
+      xargs
+    ;
+  };
+in
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    bash
+    curl
+    git
+    imagemagick
+    jq
+    openssh
+    texlive
+    pipenv
+    python39
+  ];
+}
--- shell.nix
+++ shell.nix
@@ -1,61 +1,13 @@
 let
-  pkgs = import (import ./nix/sources.nix).nixpkgs {};
-  texlive = pkgs.texlive.combine {
-    inherit (pkgs.texlive)
-      bera
-      capt-of
-      collection-fontsrecommended
-      collection-luatex
-      datetime
-      enumitem
-      environ
-      epigraph
-      eqparbox
-      eulervm
-      fancyhdr
-      fmtcount
-      fncychap
-      footmisc
-      footnotebackref
-      framed
-      latexmk
-      lipsum
-      mathpartir
-      mathpazo
-      mnsymbol
-      multirow
-      needspace
-      palatino
-      scheme-small
-      tabulary
-      threeparttable
-      tikzsymbols
-      titlesec
-      tocbibind
-      todonotes
-      transparent
-      trimspaces
-      varwidth
-      wrapfig
-      xargs
-    ;
-  };
+  pkgs = import ./nix/nixpkgs.nix;
 in
 pkgs.mkShell {
-  LOCALE_ARCHIVE_2_27 = if pkgs.stdenv.hostPlatform.libc == "glibc"
-                        then "${pkgs.glibcLocales}/lib/locale/locale-archive"
-                        else null;
-  SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
-  buildInputs = [
-    pkgs.awscli
-    pkgs.bash
-    pkgs.curl
-    pkgs.direnv
-    pkgs.git
-    pkgs.jq
-    pkgs.imagemagick
-    texlive
-    pkgs.pipenv
-    pkgs.python39
+  buildInputs = with pkgs; [
+    awscli
+    bash
+    curl
+    jq
+    git
+    nix
   ];
 }
