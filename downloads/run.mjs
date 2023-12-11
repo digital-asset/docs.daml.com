5b59eda35ab6e2ba92e137d99dabf5538b12c1d7
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2023-12-11 19:06:44 +0100

    downloads: basic page generation working

---
+++ downloads/run.mjs
@@ -0,0 +1,84 @@
+import ejs  from 'ejs';
+import fs from 'fs';
+import https from 'https';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+function request(url, options) {
+  return new Promise((resolve, reject) => {
+    const req = https.request(url, options, (res) => {
+      res.setEncoding('utf8');
+      let responseBody = '';
+
+      res.on('data', (chunk) => {
+        responseBody += chunk;
+      });
+
+      res.on('end', () => {
+        resolve(JSON.parse(responseBody));
+      });
+    });
+
+    req.on('error', (err) => {
+      reject(err);
+    });
+
+    req.end();
+  });
+}
+
+function v_url(version) {
+  const base_url = "https://digitalasset.jfrog.io/artifactory/api/storage/external-files/daml-enterprise";
+  return base_url + "/" + version.split('.')[0] + '.' + version.split('.')[1] + "/" + version;
+}
+
+async function list(version) {
+  const resp = await request(v_url(version),
+                             {auth: process.env.ARTIFACTORY_USERNAME + ":" + process.env.ARTIFACTORY_PASSWORD});
+  return resp.children.map((f) => f.uri.substring(1));
+}
+
+const components = [{name: "Daml SDK",
+                     files: [{name: "Linux Tarball", pattern: /^daml-sdk-.*-linux-ee\.tar\.gz$/},
+                             {name: "macOS Tarball", pattern: /^daml-sdk-.*-macos-ee\.tar\.gz$/},
+                             {name: "Windows Installer", pattern: /^daml-sdk-.*windows-ee\.exe$/}]},
+                    {name: "Canton", files: [{name: "Standalone JAR", pattern: /^canton-.*-ee\.jar$/}]},
+                    {name: "Daml Finance", files: [{name: "Tarball", pattern: /^daml-finance-.*-ee\.tar\.gz$/}]},
+                    {name: "Daml Script", files: [{name: "Standalone JAR", pattern: /^daml-script-.*-ee\.jar$/}]},
+                    {name: "HTTP JSON API", files: [{name: "Standalone JAR", pattern: /^http-json-.*-ee\.jar$/}]},
+                    {name: "OAuth2 Middleware", files: [{name: "Standalone JAR", pattern: /^oauth2-middleware-.*-ee\.jar$/}]},
+                    {name: "Participant Query Store", files: [{name: "Standalone JAR", pattern: /^participant-query-store-.*-ee\.jar$/}]},
+                    {name: "Trigger Runner", files: [{name: "Standalone JAR", pattern: /^trigger-runner-.*-ee\.jar$/}]},
+                    {name: "Trigger Service", files: [{name: "Standalone JAR", pattern: /^trigger-service-.*-ee\.jar$/}]}]
+
+const index_template = fs.readFileSync('index.html.template', 'utf8');
+const version_template = fs.readFileSync('version.html.template', 'utf8');
+const data = await Promise.all(fs.readFileSync('published_versions', 'utf8').split('\n').slice(0, -1).map(async (v) => {
+  const all_files = await list(v);
+  return {version: v,
+          components: components.map((c) => {
+            const files = c.files.map((f) => {
+              const file = all_files.filter((af) => af.match(f.pattern));
+              return {name: f.name,
+                      url: v_url(v) + "/" + file[0],
+                      signature: v_url + "/" + file[0] + ".asc"};
+            });
+            return {name: c.name, files};})};
+}));
+
+const index = ejs.render(index_template, {versions: data});
+const versions = data.map((v) => ({version: v.version,
+                                   rendered: ejs.render(version_template, {v: v})}));
+
+const output_dir = __dirname + '/out';
+
+if (fs.existsSync(output_dir)) {
+  fs.rmSync(output_dir, {recursive: true});
+}
+
+fs.mkdirSync(output_dir);
+fs.writeFileSync(output_dir + '/index.html', index);
+for (v of versions) {
+  fs.writeFileSync(output_dir + "/" + v.version + ".html", v.rendered);
+}
